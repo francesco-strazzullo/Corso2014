ea65d7ff6bf3b1d6843fadc5a1a024eb2e7428a1
francesco-strazzullo <me@francescostrazzullo.info>
2014-09-07 16:48:12 +0200

    Aggiunti Esempi Dao/ActiveRecord

--- patterns/src/main/java/it/strazz/corso2014/patterns/Start.java
+++ patterns/src/main/java/it/strazz/corso2014/patterns/Start.java
@@ -2,6 +2,7 @@ package it.strazz.corso2014.patterns;
 
 import it.strazz.corso2014.patterns.builder.Pizza;
 import it.strazz.corso2014.patterns.builder.PizzaBuilder;
+import it.strazz.corso2014.patterns.dao.PizzaService;
 import it.strazz.corso2014.patterns.singleton.Singleton;
 
 /**
@@ -16,7 +17,29 @@ public class Start
     {
         singletonLauncher();
         builderLauncher();
+        daoLauncher();
+        activeRecordLauncher();
     }
+
+	private static void activeRecordLauncher() {
+		System.out.println("--- Starting ActiveRecord Example ---");
+		
+		System.out.println("Pizzas: " + it.strazz.corso2014.patterns.activerecord.Pizza.getAll());
+		
+		(new it.strazz.corso2014.patterns.activerecord.Pizza(null, "Capricciosa")).store();
+
+		System.out.println("Pizzas: " + it.strazz.corso2014.patterns.activerecord.Pizza.getAll());
+	}
+
+	private static void daoLauncher() {
+		System.out.println("--- Starting DAO Example ---");
+		
+		System.out.println("Pizzas: " + PizzaService.getInstance().list());
+		
+		PizzaService.getInstance().store(new it.strazz.corso2014.patterns.dao.Pizza(null, "Capricciosa"));
+		
+		System.out.println("Pizzas: " + PizzaService.getInstance().list());
+	}
 
 	private static void builderLauncher() {
 		Pizza p1 = new PizzaBuilder().addMozzarella().addSalame().build();
